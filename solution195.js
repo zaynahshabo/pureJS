17e70251ec80355a2af56c931d50d1a2ec59bb1f
Zaynah Shabo <zdatadesign@gmail.com>
2022-10-31 07:26:53 -0700

    Add files via upload

---
+++ solution195.js
@@ -0,0 +1,28 @@
+// https://leetcode.com/explore/learn/card/fun-with-arrays/525/inserting-items-into-an-array/3245/
+// Given a fixed-length integer array arr, duplicate each occurrence of zero, shifting the remaining elements to the right.
+// Note that elements beyond the length of the original array are not written. Do the above modifications to the input array in place and do not return anything.
+// Example 1:
+// Input: arr = [1,0,2,3,0,4,5,0]
+// Output: [1,0,0,2,3,0,0,4]
+// Explanation: After calling your function, the input array is modified to: [1,0,0,2,3,0,0,4]
+// Example 2:
+// Input: arr = [1,2,3]
+// Output: [1,2,3]
+// Explanation: After calling your function, the input array is modified to: [1,2,3]
+// Constraints:
+// 1 <= arr.length <= 104
+// 0 <= arr[i] <= 9
+//receive array of integers which may or may not contain 0
+//duplicate each instance of 0, maintaining original array length and leaving off any overflowing values. do not return any values.
+//for loop through original array, if 0 is found, 
+
+let arr = [1,0,2,3,0,4,5,0]
+
+var duplicateZeros = function(arr) {
+
+}
+
+duplicateZeros(arr)
+
+
+
