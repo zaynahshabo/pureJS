79780a31a5c8515e718342fef20b64ad532e1130
Zaynah Shabo <zdatadesign@gmail.com>
2023-01-16 11:18:25 -0800

    Add files via upload

---
+++ solution270.js
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/51f41b98e8f176e70d0002a8/train/javascript
+// Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+//receive an array of strings
+//return a list sorted in lexicographical order - numbers then letters, upper then lower case
+
+const sortme = [...]names.sort()
+
+//or - non-mutating, make new array:
+
+const sortme = names => names.slice().sort()
+
+
+
+
+
