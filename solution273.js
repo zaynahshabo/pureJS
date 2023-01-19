5f6b26c1bfed6922525e109b8b4aa4b9d6744989
Zaynah Shabo <zdatadesign@gmail.com>
2023-01-19 06:52:39 -0800

    Add files via upload

---
+++ solution273.js
@@ -0,0 +1,58 @@
+// https://www.codewars.com/kata/5aa1bcda373c2eb596000112/train/javascript
+// Given an array/list [] of n integers , find maximum triplet sum in the array Without duplications .
+
+// Notes :
+// Array/list size is at least 3 .
+
+// Array/list numbers could be a mixture of positives , negatives and zeros .
+
+// Repetition of numbers in the array/list could occur , So (duplications are not included when summing).
+
+// Input >> Output Examples
+// 1- maxTriSum ({3,2,6,8,2,3}) ==> return (17)
+// Explanation:
+// As the triplet that maximize the sum {6,8,3} in order , their sum is (17)
+
+// Note : duplications are not included when summing , (i.e) the numbers added only once .
+
+// 2- maxTriSum ({2,1,8,0,6,4,8,6,2,4}) ==> return (18)
+// Explanation:
+// As the triplet that maximize the sum {8, 6, 4} in order , their sum is (18) ,
+
+// Note : duplications are not included when summing , (i.e) the numbers added only once .
+
+// 3- maxTriSum ({-7,12,-7,29,-5,0,-7,0,0,29}) ==> return (41)
+// Explanation:
+// As the triplet that maximize the sum {12 , 29 , 0} in order , their sum is (41) ,
+
+// Note : duplications are not included when summing , (i.e) the numbers added only once .
+
+
+
+//receive an array of n integers 
+//return the max triplet sum - without any duplicate values
+//STEPS:
+//sort array, push values to a new array
+//create a nonduplicate- holder array, and use includes() to test if they are unique values- only push the unique values to second array
+//sum three highest from the second array
+
+
+
+let numbers = [-13,-50,57,13,67,-13,57,108,67]
+
+function maxTriSum(numbers){
+    let sorted = numbers.sort((a,z) => z - a)
+    let nonDups = []
+    
+    for (let i = 0; i < sorted.length; i++) {
+        if (nonDups.includes(sorted[i]) === false) {
+            nonDups.push(sorted[i])
+        }
+    }return nonDups[0] + nonDups[1] + nonDups[2]
+}
+
+maxTriSum(numbers)
+
+
+
+
