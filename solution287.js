9123c34ca1c1eacc821ef88b0446657ca0eb1f19
Zaynah Shabo <zdatadesign@gmail.com>
2023-02-01 07:55:03 -0800

    Add files via upload

---
+++ solution287.js
@@ -0,0 +1,30 @@
+// https://www.codewars.com/kata/5966e33c4e686b508700002d/train/javascript
+// Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+// Example: (Input1, Input2 -->Output)
+// "4",  "5" --> "9"
+// "34", "5" --> "39"
+// "", "" --> "0"
+// "2", "" --> "2"
+// "-5", "3" --> "-2"
+// Notes:
+// If either input is an empty string, consider it as zero.
+// Inputs and the expected output will never exceed the signed 32-bit integer limit (2^31 - 1)
+
+//receive two integers as strings
+//return their sum, expressed as a string
+//STEPS: convert each value to a number, add, return value as a string
+
+
+let a = "4"
+let b = "5"
+function sumStr(a,b) {
+    return String(Number(a) + Number(b))
+}
+
+sumStr(a,b)
+
+
+
+
+
+
